cd31e5acefd27a50ef84dbe3d45f885b539d7c50
gabriel <gabriel@gabriel-VirtualBox>
2020-03-06 20:39:30 +0100

    hhhhkjjhk

--- src/fichas/Ficha.java
+++ src/fichas/Ficha.java
@@ -8,10 +8,10 @@ public abstract class Ficha  {
 	public static enum Shape{
 		//enum de las formas posibles que pueden tomar las fichas
 		
-		white_queen("\u2655",Color.white),white_king("\2654",Color.white),white_bishop("\2657",Color.white),
-		white_rook("\2656",Color.white),white_knight("\2658",Color.white),white_pawn("\2659",Color.white)
-		,black_queen("\u265B",Color.black),black_king("\u265A",Color.black),black_bishop("\265D",Color.black),
-		black_rook("\u265C",Color.black),black_knight("\265E",Color.black),black_pawn("\u265F",Color.black);
+		white_queen("\u2655",Color.white),white_king("\u2654",Color.white),white_bishop("\u2657",Color.white),
+		white_rook("\u2656",Color.white),white_knight("\u2658",Color.white),white_pawn("\u2659",Color.white)
+		,black_queen("\u265B",Color.black),black_king("\u265A",Color.black),black_bishop("\u265D",Color.black),
+		black_rook("\u265C",Color.black),black_knight("\u265E",Color.black),black_pawn("\u265F",Color.black);
 		public Color color;
 		private String forma;
 	
