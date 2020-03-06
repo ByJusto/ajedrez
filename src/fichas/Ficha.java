c77cfa0576ee6d0aaa3559eeb34d455f525e78d1
Gabriel <Gabriel@DESKTOP-1DTHM8O>
2020-03-06 20:10:43 +0100

    mkk

--- src/fichas/Ficha.java
+++ src/fichas/Ficha.java
@@ -6,14 +6,14 @@ import java.util.Iterator;
 public abstract class Ficha  {
 
 	public static enum Shape{
-		white_queen("Q",Color.white),white_king("K",Color.white),white_bishop("B",Color.white),
+		/*white_queen("Q",Color.white),white_king("K",Color.white),white_bishop("B",Color.white),
 		white_rook("T",Color.white),white_knight("C",Color.white),white_pawn("P",Color.white)
 		,black_queen("Q",Color.black),black_king("K",Color.black),black_bishop("B",Color.black),
-		black_rook("T",Color.black),black_knight("C",Color.black),black_pawn("P",Color.black);
-		/*white_queen("\u2655",Color.white),white_king("\2654",Color.white),white_bishop("\2657",Color.white),
+		black_rook("T",Color.black),black_knight("C",Color.black),black_pawn("P",Color.black);*/
+		white_queen("\u2655",Color.white),white_king("\2654",Color.white),white_bishop("\2657",Color.white),
 		white_rook("\2656",Color.white),white_knight("\2658",Color.white),white_pawn("\2659",Color.white)
 		,black_queen("\u265B",Color.black),black_king("\u265A",Color.black),black_bishop("\265D",Color.black),
-		black_rook("\u265C",Color.black),black_knight("\265E",Color.black),black_pawn("\u265F",Color.black);*/
+		black_rook("\u265C",Color.black),black_knight("\265E",Color.black),black_pawn("\u265F",Color.black);
 		public Color color;
 		private String forma;
 	
